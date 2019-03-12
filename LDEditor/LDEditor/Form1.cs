6eae1927c883dffabb1198a8e12be21016a05559
Александр Анисимов <skier98@list.ru>
2019-03-12 22:47:57 +0300

    menu/toolbar/status

--- LDEditor/LDEditor/Form1.cs
+++ LDEditor/LDEditor/Form1.cs
@@ -15,25 +15,13 @@ namespace LDEditor
         public Main()
         {
             InitializeComponent();
-            LDlabel1.Text = "Графический редактор";
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Открыт файл");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void LDlabel1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
 
         }
